a545d462ffb9abe73231ed4f0efcc63fd3fcc12a
Jey3003 <andre.d3003@gmail.com>
2020-08-10 17:33:59 +0200

    refactor app helper no nesting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,18 +4,18 @@ module ApplicationHelper
   BLOCK_AVATAR = "https://68.media.tumblr.com/9f9b498bf798ef43dddeaa78cec7b027/tumblr_o51oavbMDx1ugpbmuo7_500.png"
   TRIANGLE_AVATAR = "http://78.media.tumblr.com/75bad14fee104f69652084d545213291/tumblr_mldfty8fh41qcnibxo5_1280.png"
   HAT_AVATAR = "https://68.media.tumblr.com/22d1c50c3e2ca1062a94b47a65bfeb6d/tumblr_o51oavbMDx1ugpbmuo10_500.png"
+
   def default_avatar(user)
-    if user.name.present?
-      username = user.name[0].downcase
-      case(username)
-      when 'a'..'h' then  CAT_AVATAR
-      when 'i'..'n' then  BLOCK_AVATAR
-      when 'o'..'u'  then  TRIANGLE_AVATAR
-      else    HAT_AVATAR
-      end
-    else
-      CAT_AVATAR
+    
+    user.name[0].downcase = CAT_AVATAR if user.name.nil?
+
+    case(user.name[0].downcase)
+    when 'a'..'h' then  CAT_AVATAR
+    when 'i'..'n' then  BLOCK_AVATAR
+    when 'o'..'u'  then  TRIANGLE_AVATAR
+    else    HAT_AVATAR
     end
+
   end
 
   def pluralize_comments(comments)
