f594fc224c5438faed438d00a315fc07c1aba39f
Jey3003 <andre.d3003@gmail.com>
2020-08-11 13:12:14 +0200

    paginate tips

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -9,10 +9,12 @@ class Tip < ApplicationRecord
   validates :body,  presence: true
 
 
-    scope :most_recent, -> {order(updated_at: :desc).limit(4)}
+  scope :most_recent, -> {order(updated_at: :desc).limit(4)}
 
-    scope :title_contains,       ->(term) { where('title LIKE ?', "%#{term}%") }
-    scope :description_contains, ->(term) { where('body LIKE ?', "%#{term}%") }
+  scope :title_contains,       ->(term) { where('title LIKE ?', "%#{term}%") }
+  scope :description_contains, ->(term) { where('body LIKE ?', "%#{term}%") }
 
-    scope :search, ->(search_term) { title_contains(search_term).or(description_contains(search_term)) }
+  scope :search, ->(search_term) { title_contains(search_term).or(description_contains(search_term)) }
+
+  paginates_per 8
 end
