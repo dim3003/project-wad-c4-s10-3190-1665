4790ea80a03322df31c25fb867f7002fd46a8ed4
Jey3003 <andre.d3003@gmail.com>
2020-08-11 12:19:58 +0200

    scopes

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,10 +1,18 @@
 class Tip < ApplicationRecord
   belongs_to :user
   has_many   :comments
-  
+
   has_and_belongs_to_many :users
 
   validates :title, presence: true,
                     length: { maximum: 100 }
   validates :body,  presence: true
+
+
+    scope :most_recent, -> {order(updated_at: :desc).limit(4)}
+
+    scope :title_contains,       ->(term) { where('title LIKE ?', "%#{term}%") }
+    scope :description_contains, ->(term) { where('body LIKE ?', "%#{term}%") }
+
+    scope :search, ->(search_term) { title_contains(search_term).or(description_contains(search_term)) }
 end
