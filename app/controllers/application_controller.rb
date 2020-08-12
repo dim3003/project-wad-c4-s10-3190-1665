ef6dd6f3b29307b0f18cd5dad2454e18d69afa00
Jey3003 <andre.d3003@gmail.com>
2020-08-12 14:31:05 +0200

    ensure authenticated helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,10 @@ class ApplicationController < ActionController::Base
     @current_user = User.find(session[:user_id])
   end
 
+  def ensure_authenticated
+    unless(logged_in?)
+      redirect_to login_path
+    end
+  end
+
 end
